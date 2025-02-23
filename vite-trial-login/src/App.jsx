1e68a578833f48dee03825bda3cca32411e7e86f
snigudkar <146969559+snigudkar@users.noreply.github.com>
2025-02-23 22:15:18 +0530

    first

--- vite-trial-login/src/App.jsx
+++ vite-trial-login/src/App.jsx
@@ -1,38 +1,4 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
 import './App.css';
 
 import React, { useState } from "react";
@@ -48,14 +14,9 @@ import { initializeApp } from "firebase/app";
 import "./App.css";
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAuNwvEfgeTMpJdEZhVkWJpX0PjZwdXQVM",
-  authDomain: "login-page-923f8.firebaseapp.com",
-  projectId: "login-page-923f8",
-  storageBucket: "login-page-923f8.firebasestorage.app",
-  messagingSenderId: "784857233109",
-  appId: "1:784857233109:web:594bb2dfe13780a0e4545c"
-};
+//INSERT API DETAILS HERE
+//
+//
 
 initializeApp(firebaseConfig);
 const auth = getAuth();
